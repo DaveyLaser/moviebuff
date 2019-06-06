8f843fbfa041480b0c3c7b5361d494f0ffa0214d
Dave Miller <dm@enfuse.io>
2019-06-06 11:26:25 -0600

    Fix tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from '../containers/App'
+import Root from '../containers/Root'
 
 describe('Simple render tests', () => {
   test('App renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
+    ReactDOM.render(<Root />, div)
   })
 })
