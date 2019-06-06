51d32825ac61f57b1a91e4e0b49c6c3f7dbb723c
Dave Miller <dm@enfuse.io>
2019-06-06 12:14:06 -0600

    Update Banner component to functional style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,16 +11,12 @@ const bannerStyle = {
 
 const iconCount = 3
 
-class Banner extends React.Component {
-  render() {
-    return (
-      <div className="Banner" style={bannerStyle}>
-        {[...Array(iconCount)].map((x, i) =>
-          <img src="favicon.ico" alt="n/a" key={i}></img>
-        )}
-      </div>
-    )
-  }
-}
+const Banner = () => (
+  <div className="Banner" style={bannerStyle}>
+    {[...Array(iconCount)].map((x, i) =>
+      <img src="favicon.ico" alt="n/a" key={i}></img>
+    )}
+  </div>
+)
 
 export default Banner
